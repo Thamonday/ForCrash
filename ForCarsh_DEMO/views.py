cc06ab4ea5fb7b84b848f7af3a8e7bc203fe6f68
Thamonday <thamonwanrosi1@gmail.com>
2022-04-09 08:34:28 +0700

    Deploy project to Heroku

--- ForCarsh_DEMO/views.py
+++ ForCarsh_DEMO/views.py
@@ -100,11 +100,14 @@ def postregister(request):
     return render(request,'Login.html')
 
 def postQuestionForm(request):
-    with open('./models/sav_model2_xgboost.sav','rb') as f:
-        clf1 = pickle.load(f)
-    with open('./models/sav_model2_xgboost.sav','rb') as f:
-        clf2 = pickle.load(f)
-    gaussian = pickle.load(open('./models/sav_model2_xgboost.sav','rb'))
+    # loaded_model = pickle.load(open('./models/sav_model2_xgboost.sav', "rb"))
+    clf1 = pickle.load(open('./models/sav_model2_xgboost.sav','rb'))
+    clf2 = pickle.load(open('./models/sav_model2_xgboost.sav','rb'))
+    # # with open('./models/sav_model2_xgboost.sav','rb') as f:
+    # #     clf1 = pickle.load(f)
+    # with open('./models/sav_model2_xgboost.sav','rb') as f:
+    #     clf2 = pickle.load(f)
+    # clf1 = pickle.load(open('./models/sav_model2_xgboost.sav','rb'))
     day_of_week = date_now.strftime("%A")
     acc_month = int(date_now.strftime("%m"))
     acc_time = int(date_now.strftime("%H"))
